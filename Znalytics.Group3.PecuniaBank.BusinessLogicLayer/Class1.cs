ff82c75f69a641b6adfdca26fa80f6dd79be01c2
Manasa-12 <ManasaA@agilitycis.com>
2020-08-14 14:26:45 +0530

    act

--- Znalytics.Group3.PecuniaBank.BusinessLogicLayer/Class1.cs
+++ Znalytics.Group3.PecuniaBank.BusinessLogicLayer/Class1.cs
@@ -1,5 +1,15 @@
+
 ﻿public class Bank
 {
     private string _actHolderName;
+
+}
+
+namespace Znalytics.Group3.PecuniaBank.BusinessLogicLayer
+{
+    public class Customer
+    {
+        public string name;
+    }
 
 }
