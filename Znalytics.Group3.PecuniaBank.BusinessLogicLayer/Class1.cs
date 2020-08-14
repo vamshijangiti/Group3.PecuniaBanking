e7984060e45a9913c4ed83c87acf27ddf03491dc
Manasa-12 <ManasaA@agilitycis.com>
2020-08-14 14:16:07 +0530

    Account holder name

--- Znalytics.Group3.PecuniaBank.BusinessLogicLayer/Class1.cs
+++ Znalytics.Group3.PecuniaBank.BusinessLogicLayer/Class1.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group3.PecuniaBank.BusinessLogicLayer
+﻿public class Bank
 {
-    public class Class1
-    {
-    }
+    private string _actHolderName;
+
 }
