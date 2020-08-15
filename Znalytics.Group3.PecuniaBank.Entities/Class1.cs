f01050c423ad7b69914152dcdae04712a31d1177
srirammartha <srirammartha36@gmail.com>
2020-08-15 10:18:02 +0530

    hi

--- Znalytics.Group3.PecuniaBank.Entities/Class1.cs
+++ Znalytics.Group3.PecuniaBank.Entities/Class1.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group3.PecuniaBank.Entities
-{
-    public class Class1
-    {
-    }
-}
+﻿
