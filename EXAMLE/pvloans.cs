bfca93a957600303aa77b44101f469a17764b3ba
Navya <donthamnavya31@gmail.com>
2020-08-15 15:43:11 +0530

    loans

---
+++ EXAMLE/pvloans.cs
@@ -0,0 +1,102 @@
+﻿class Program
+{
+    static void Main()
+    {
+        PersonalLoan P;
+        P = new PersonalLoan();
+        VehicleLoan v;
+        v = new VehicleLoan();
+
+        System.Console.WriteLine("WELCOME TO PECUNIA BANK");
+        System.Console.WriteLine("FOR LOAN ACCEPTANCE");
+        System.Console.WriteLine("1 - FOR PERSONAL LOAN");
+        System.Console.WriteLine("2 - FOR VEHICLE LOAN");
+        System.Console.WriteLine("3-EXIT");
+
+
+        int n;
+        n = int.Parse(System.Console.ReadLine());
+
+
+
+        {
+            switch (n)
+            {
+                case 1:
+
+                    System.Console.WriteLine("Bank Account Number for personal loan");
+                    P.SetAccountNumber(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetAccountNumber());
+
+                    System.Console.WriteLine("Enter the Bank Holders Name");
+                    P.SetAccountHolderName(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetAccountHolderName());
+
+                    System.Console.WriteLine("Enter the PhoneNumber");
+                    P.SetPhoneNumber(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetPhoneNumber());
+
+                    System.Console.WriteLine("Enter the Permanent Address");
+                    P.SetPermanentAddress(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetPermanentAddress());
+
+                    System.Console.WriteLine("Enter the PanCardNumber");
+                    P.SetPanCardNumber(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetPanCardNumber());
+
+                    System.Console.WriteLine("Enter the Profession");
+                    P.SetProfession(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetProfession());
+
+                    System.Console.WriteLine("Enter the AnnualIncome");
+                    P.SetAnnualIncome(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetAnnualIncome());
+
+                    System.Console.WriteLine("Enter the LoanAmount");
+                    P.SetLoanAmount(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetLoanAmount());
+                    break;
+
+                case 2:
+                    System.Console.Write("Enter the Bank Account Number for vehicle loan");
+                    P.SetAccountNumber(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetAccountNumber());
+
+                    System.Console.WriteLine("Enter the Bank Holders Name");
+                    P.SetAccountHolderName(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetAccountHolderName());
+
+                    System.Console.WriteLine("Enter the PhoneNumber");
+                    P.SetPhoneNumber(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetPhoneNumber());
+
+                    System.Console.WriteLine("Enter the Permanent Address");
+                    P.SetPermanentAddress(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetPermanentAddress());
+
+                    System.Console.WriteLine("Enter the PanCardNumber");
+                    P.SetPanCardNumber(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetPanCardNumber());
+
+                    System.Console.WriteLine("Enter the Profession");
+                    P.SetProfession(System.Console.ReadLine());
+                    System.Console.WriteLine(P.GetProfession());
+
+                    System.Console.WriteLine("Enter the AnnualIncome");
+                    P.SetAnnualIncome(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetAnnualIncome());
+
+                    System.Console.WriteLine("Enter the LoanAmount");
+                    P.SetLoanAmount(double.Parse(System.Console.ReadLine()));
+                    System.Console.WriteLine(P.GetLoanAmount());
+
+                    break;
+                case 3:
+                    System.Console.WriteLine("Thanks For Using PECUNIA BANK");
+                    break;
+            }
+        }
+        System.Console.ReadKey();
+    }
+}
+
