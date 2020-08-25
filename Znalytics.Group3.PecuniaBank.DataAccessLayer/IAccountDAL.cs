7022b5cc411ba1fc87b273f8c292a2f7e8bf2185
vamshijangiti <vamshijangiti@gmail.com>
2020-08-25 21:42:12 +0530

    hello

---
+++ Znalytics.Group3.PecuniaBank.DataAccessLayer/IAccountDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group3.PecuniaBank.DataAccessLayer
+{
+    class IAccountDAL
+    {
+    }
+}
