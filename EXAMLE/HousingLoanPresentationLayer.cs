6fcd0a36334197a55b9c3a7401cb5d3da59dd7c8
srirammartha <srirammartha36@gmail.com>
2020-08-28 11:51:05 +0530

    hi all cnges

--- EXAMLE/HousingLoanPresentationLayer.cs
+++ EXAMLE/HousingLoanPresentationLayer.cs
@@ -5,66 +5,64 @@ using Znalytics.Group3.PecuniaBank.Entities;
 namespace Znalytics.Group3.PecuniaBank.HousingAndJewelPresentationLayer
 {
     /// <summary>
-    /// PresentationLayer for HousingLoan and JewelLoan
+    /// PresentationLayer for HousingLoan
     /// </summary>
     class program
     {
-        static void Main()
+        public static void HousingLoanPresentationLayer()
         {
-
-            System.Console.WriteLine("-----welcome to pecunia bank-------");//options for type of loan
-            System.Console.WriteLine("1.HousingLoan");
-            System.Console.WriteLine("2.exit");
-            int n;
-            n = int.Parse(System.Console.ReadLine());//using n variable in the switch case
+            int choice = 0;
+            do
             {
-                switch (n)
+
+                System.Console.WriteLine("WELCOME TO PECUNIA BANKING");
+                System.Console.WriteLine("Menu");
+                System.Console.WriteLine("1. Approve HouseLoan ");
+                System.Console.WriteLine("2.Pending request for HouseLoan");
+                System.Console.WriteLine("3.rejectHouseLoan");
+                System.Console.WriteLine("4. Exit");
+                System.Console.WriteLine("Enter the choice: ");
+                choice = int.Parse(System.Console.ReadLine());
+                switch (choice)
                 {
-                    case 1://choosing HousingLoan option
-                        HousingLoan h = new HousingLoan();
-                        System.Console.WriteLine("enter accountno");//prints account no
-                        long _accountNo = long.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter Age of Customer");
-                        int _ageOfCustomer = int.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter credit score");
-                        int _creditScore = int.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter Loan Amount");
-                        int _loanOfAmount = int.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter rateofinterest");
-                        double _rateOfInterest = double.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter tenure");
-                        double _tenure = double.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("emi value");
-                        float _emi = float.Parse(System.Console.ReadLine());
 
-
-                        break;
-
-                    /*case 2://choosing GoldLoan option
-                        JewelLoan g = new JewelLoan();
-                        System.Console.WriteLine("enter aadharno");
-                        long _aadharNo = long.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter ageofcustomer");
-                        int ageOfCustomer = int.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter Loan Amount");
-                        int loanOfAmount = int.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter rateofinterest");
-                        double rateOfInterest = double.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("enter tenure");
-                        double tenure = double.Parse(System.Console.ReadLine());
-                        System.Console.WriteLine("emi value");
-                        float emi = float.Parse(System.Console.ReadLine());
-
-                        break;*/
-                    case 2:
-                        System.Console.WriteLine("exit ");
-                        break;
                 }
+                System.Console.WriteLine("-----welcome to pecunia bank-------");//options for type of loan
+                System.Console.WriteLine("1.HousingLoan");
+                System.Console.WriteLine("2.exit");
+                int n;
+                n = int.Parse(System.Console.ReadLine());//using n variable in the switch case
+                {
+                    switch (n)
+                    {
+                        case 1://choosing HousingLoan option
+                            HousingLoan h = new HousingLoan();
+                            System.Console.WriteLine("enter accountno");//prints account no
+                            long _accountNo = long.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("enter Age of Customer");
+                            int _ageOfCustomer = int.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("enter credit score");
+                            int _creditScore = int.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("enter Loan Amount");
+                            int _loanOfAmount = int.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("enter rateofinterest");
+                            double _rateOfInterest = double.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("enter tenure");
+                            double _tenure = double.Parse(System.Console.ReadLine());
+                            System.Console.WriteLine("emi value");
+                            float _emi = float.Parse(System.Console.ReadLine());
 
-                System.Console.ReadKey();
+
+                            break;
+
+                        case 2:
+                            System.Console.WriteLine("exit ");
+                            break;
+                    }
+
+                    System.Console.ReadKey();
+                }
             }
-        }
+   }
     }
-}
- 
         
