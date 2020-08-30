f9ffe544135e8e0c1a023f9e083dd408b2fdedc8
Navya <donthamnavya31@gmail.com>
2020-08-30 21:28:58 +0530

    chng

---
+++ Znalytics.Group3.PecuniaBank.Entities/VehicleLoanException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group3.PecuniaBank.Entities
+{
+
+    public class VehicleLoanException : ApplicationException
+    {
+        public VehicleLoanException() : base()
+        {
+
+        }
+        public VehicleLoanException(string message) : base(message)
+        {
+
+        }
+        public VehicleLoanException(string message, Exception innerException) : base(message)
+        {
+
+        }
+
+    }
+
+
+}
