ad1eebac3f99ee784c59efb8c0d00d5609a4d7b2
Navya <donthamnavya31@gmail.com>
2020-08-30 20:45:33 +0530

    chng

--- Znalytics.Group3.PecuniaBank.Entities/PersonalLoanEntity.cs
+++ Znalytics.Group3.PecuniaBank.Entities/PersonalLoanEntity.cs
@@ -52,20 +52,26 @@ namespace Znalytics.Group3.PecuniaBank.Entities
         {
             set
             {
-                if (value.Length == 6)
+                try
                 {
-                    _accountNumber = value;
+                    if (value.Length == 6)
+                    {
+                        _accountNumber = value;
+                    }
+
+                    else
+                    {
+                        throw new Exception("Enter 6 digits only\n");
+                    }
+
                 }
-
-
-
-
-                else
+                catch
                 {
-                    throw new Exception("Enter 6 digits only\n");
+                    throw;
                 }
 
             }
+
             get
             {
                 return _accountNumber;
@@ -78,17 +84,24 @@ namespace Znalytics.Group3.PecuniaBank.Entities
         {
             set
             {
-                if (value >= 50000)
+                try
                 {
-                    _loanAmount = value;
-                }
+                    if (value >= 50000)
+                    {
+                        _loanAmount = value;
+                    }
 
-                else
+                    else
+                    {
+                        throw new Exception("not eligible\n");
+
+                    }
+
+                }
+                catch
                 {
-                    throw new Exception("not eligible\n");
-
+                    throw;
                 }
-
             }
             get
             {
@@ -101,15 +114,23 @@ namespace Znalytics.Group3.PecuniaBank.Entities
         {
             set
             {
-                if (value >= 10)
+                try
                 {
-                    _tenure = value;
+                    if (value >= 10)
+                    {
+                        _tenure = value;
+                    }
+
+                    else
+                    {
+                        throw new Exception("not eligible\n");
+
+                    }
+
                 }
-
-                else
+                catch
                 {
-                    throw new Exception("not eligible\n");
-
+                    throw;
                 }
 
             }
---
+++ Znalytics.Group3.PecuniaBank.Entities/PersonalLoanException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group3.PecuniaBank.Entities
+{
+    
+        public class PersonalLoanException:ApplicationException
+        {
+            public PersonalLoanException():base()
+            {
+
+            }
+            public PersonalLoanException (string message):base(message)
+            {
+
+            }
+            public PersonalLoanException (string message,Exception innerException):base(message)
+            {
+
+            }
+
+        }
+          
+    
+}
