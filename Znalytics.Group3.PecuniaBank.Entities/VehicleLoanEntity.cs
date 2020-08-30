da8d352222d12b50ad130e38c9c893b4a43fed27
Navya <donthamnavya31@gmail.com>
2020-08-30 21:04:41 +0530

    chng

--- Znalytics.Group3.PecuniaBank.Entities/VehicleLoanEntity.cs
+++ Znalytics.Group3.PecuniaBank.Entities/VehicleLoanEntity.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Znalytics.Group3.PecuniaBank.AccountEntities
+namespace Znalytics.Group3.PecuniaBank.Entities
 {
 
     /// <summary>
