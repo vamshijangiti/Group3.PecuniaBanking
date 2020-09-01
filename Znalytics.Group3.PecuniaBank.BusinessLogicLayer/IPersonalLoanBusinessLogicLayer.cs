e9c50ecd89308acc0d0a8ebbbcd226b42b6fd7ac
Navya <donthamnavya31@gmail.com>
2020-09-01 22:57:05 +0530

    interface

---
+++ Znalytics.Group3.PecuniaBank.BusinessLogicLayer/IPersonalLoanBusinessLogicLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Znalytics.Group3.PecuniaBank.Entities;
+
+namespace Znalytics.Group3.PecuniaBank.BusinessLogicLayer
+{
+    interface IPersonalLoanBusinessLogicLayer
+    {
+        int CreditScore(PersonalLoanEntity p);
+        float CalculateEmi(PersonalLoanEntity p);
+        void AddPersonalLoan(PersonalLoanEntity pl);
+    }
+}
---
+++ Znalytics.Group3.PecuniaBank.BusinessLogicLayer/IVehicleLoanBusinessLogicLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Znalytics.Group3.PecuniaBank.Entities;
+
+namespace Znalytics.Group3.PecuniaBank.BusinessLogicLayer
+{
+    interface IVehicleLoanBusinessLogicLayer
+    {
+        int CreditScore(VehicleLoanEntity p);
+        float CalculateEmi(VehicleLoanEntity p);
+        void AddPersonalLoan(VehicleLoanEntity pl);
+
+    }
+}
